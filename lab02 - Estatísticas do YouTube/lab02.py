27ce4802f43bfc0f071b85b90dbdda24a1518381
Isabella Rigue <bellarigue@gmail.com>
2021-12-26 22:38:35 -0300

    otimiza arquivos

--- lab02 - Estatísticas do YouTube/lab02.py
+++ lab02 - Estatísticas do YouTube/lab02.py
@@ -1,92 +1,51 @@
 nome_canal = str(input())
 n = int(input())
 
-lista1 = []
-data_separada1 = [] 
-i = 0
-j = 0
-valor2018 = 0
-valor2018T = 0
-valor2019 = 0
-valor2019T = 0
-valor2020 = 0
-valor2020T = 0
-contador2018 = 0
-contador2019 = 0
-contador2020 = 0
+contador = v2018 = v2019 = v2020 = total2018 = total2019 = total2020 = 0
+while contador < n:
+    data = str(input()).split("-")
+    visualizacoes = int(input())
 
-while i < 2*n:
-    lista1.append (str(input()))
-    i += 1
+    if data[0] == "2018":
+        v2018 += visualizacoes
+        total2018 += 1
+    elif data[0] == "2019":
+        v2019 += visualizacoes
+        total2019 += 1
+    elif data[0] == "2020":
+        v2020 += visualizacoes
+        total2020 += 1
 
-i = 0
-while i < 2*n:
-    data_separada1 =lista1[i].split('-')
-    j =  data_separada1[0].split('-')
-    t = int(j[0])
-    
-    if ( t < 2018 or t > 2020): 
-        i = i + 2
-    elif (t == 2018):
-        i = i + 1
-        valor2018 = (lista1[i])
-        valor2018T = valor2018T + int(valor2018)
-        contador2018 += 1
-        i += 1
-    elif (t == 2019):
-        i = i + 1
-        valor2019 = (lista1[i])
-        valor2019T = valor2019T + int(valor2019)
-        contador2019 += 1
-        i += 1
-    else:
-        i = i + 1
-        valor2020 = (lista1[i])
-        valor2020T = valor2020T + int(valor2020)
-        contador2020 += 1
-        i += 1
+    contador+= 1
 
-valor2018T = round (valor2018T, 2)
-valor2019T = round (valor2019T, 2)
-valor2020T = round (valor2020T, 2)
+v2018 = round(v2018, 2)
+v2019 = round(v2019, 2)
+v2020 = round(v2020, 2)
 
-total_trienio = valor2018T + valor2019T + valor2020T
-
+total_trienio = v2018 + v2019 + v2020
 if total_trienio != 0:
-    porcentagem2018 = (valor2018T * 100)/ total_trienio
-    porcentagem2019 = (valor2019T * 100)/ total_trienio
-    porcentagem2020 = (valor2020T * 100)/ total_trienio
+    porcentagem2018 = (v2018 * 100)/ total_trienio
+    porcentagem2019 = (v2019 * 100)/ total_trienio
+    porcentagem2020 = (v2020 * 100)/ total_trienio
 else:
-    porcentagem2018 = str('indeterminada')
-    porcentagem2019 = str('indeterminada')
-    porcentagem2020 = str('indeterminada')
+    porcentagem2018 = porcentagem2019 = porcentagem2020 = 'indeterminada'
 
-media_trienio = total_trienio/ n
-media2018 = valor2018T/ contador2018
-media2019 = valor2019T/ contador2019
-media2020 = valor2020T/ contador2020
-
-print ('Canal:', nome_canal)
-print ('Total de views do trienio:', total_trienio)
-print ('Media de views do trienio: {:.2f}'.format(media_trienio))
-print ( )
-print ('2018')
-print ('Total:', valor2018T)
-print ('Porcentagem das views do trienio: {:.2f}'.format(porcentagem2018))
-print ('Media: {:.2f}'.format(media2018))
-print ( )
-print ('2019')
-print ('Total:', valor2019T)
-print ('Porcentagem das views do trienio: {:.2f}'.format(porcentagem2019))
-print ('Media: {:.2f}'.format(media2019))
-print ( )
-print ('2020')
-print ('Total:', valor2020T)
-print ('Porcentagem das views do trienio: {:.2f}'.format(porcentagem2020))
-print ('Media: {:.2f}'.format(media2020))
-
-
-
-
-
+print('Canal:', nome_canal)
+print('Total de views do trienio:', total_trienio)
+print('Media de views do trienio: {:.2f}'.format(total_trienio / (total2018 + total2019 + total2020)))
+print("")
+print('2018')
+print('Total:', v2018)
+print('Porcentagem das views do trienio: {:.2f}'.format(porcentagem2018))
+print('Media: {:.2f}'.format(v2018 / total2018))
+print("")
+print('2019')
+print('Total:', v2019)
+print('Porcentagem das views do trienio: {:.2f}'.format(porcentagem2019))
+print('Media: {:.2f}'.format(v2019 / total2019))
+print("")
+print('2020')
+print('Total:', v2020)
+print('Porcentagem das views do trienio: {:.2f}'.format(porcentagem2020))
+print('Media: {:.2f}'.format(v2020 / total2020))
 
--- lab03 - Highlight moves das peças de Xadrez/lab03.py
+++ lab03 - Highlight moves das peças de Xadrez/lab03.py
@@ -1,189 +1,117 @@
 n = int(input())
-lista1 = []
-tabuleiro = []
-dados = []
-nome = []
-posicao_letra = []
-posicao_num = []
-casa = [ ]
-numero = [ ]
-m = 0
-
-#separando os dados em listas:
-i=0
 while n != 0:
-    tabuleiro.append (n)
-    lista1.append (str(input()))
-    n = int(input())
-    dados = lista1[i].split(' ')
-    nome.append (dados[0])
-    posicao_letra.append (ord(dados[1]))
-    posicao_num.append (int(dados[2]))
-    i += 1
-while m < len(posicao_num):
-    casa.append(chr(posicao_letra[m]))
-    numero.append (str(posicao_num[m]))
-    m +=1
+    tabuleiro = n
+    entrada = input().split(" ")
+    peca = entrada[0]
+    coluna = entrada[1]
+    posicao_coluna_ord = ord(coluna)
+    posicao_linha = int(entrada[2])
 
-j = 0
-while j < len(tabuleiro):
-    n = tabuleiro [j]
-    print ('Movimentos para a peca',nome[j],'a partir da casa',casa[j]+ numero[j]+'.') 
-    if nome [j] == 'Torre':
-            for linha in range (tabuleiro[j]):
-                print(n,end=' ')
-                n -=1
-                for coluna in range (2, (tabuleiro[j]+2), 1):
-                    if coluna != (posicao_letra[j] - 95) and posicao_num [j] == (n+1):
+    print(f'Movimentos para a peca {peca} a partir da casa {coluna}{posicao_linha}.')
+    for linha in range(n):
+        print(tabuleiro, end=' ')
+        tabuleiro -= 1
+        for coluna in range(2, (n + 2), 1):
+            if peca == "Torre":
+                if coluna != (posicao_coluna_ord - 95) and posicao_linha == (tabuleiro + 1):
+                    print('x', end= ' ')
+                elif (tabuleiro + 1) == posicao_linha and coluna == (posicao_coluna_ord - 95):
+                    print('o', end= ' ')
+                elif coluna == (posicao_coluna_ord - 95) and (tabuleiro + 1) != posicao_linha:
+                    print('x', end= ' ')
+                else:
+                    print('-', end= ' ')
+            elif peca == "Bispo":
+                posicaoxl = (tabuleiro + 1) - posicao_linha
+                if tabuleiro + 1 != posicao_linha:
+                    if (coluna - 1) == (posicao_coluna_ord - 96 - posicaoxl):
                         print('x', end= ' ')
-                    elif (n + 1) == posicao_num [j] and coluna == (posicao_letra[j] - 95):
-                        print('o', end= ' ')
-                    elif coluna == (posicao_letra[j] - 95) and (n + 1) != posicao_num [j]:
+                    elif (coluna - 1) == (posicao_coluna_ord - 96 + posicaoxl):
                         print('x', end= ' ')
+                    else:
+                            print('-', end= ' ')
+                else:
+                    if (coluna - 1) == (posicao_coluna_ord - 96):
+                            print('o', end= ' ')
                     else:
                         print('-', end= ' ')
-                print ( )
-            print (' ', end=' ')    
-            for linha in range (tabuleiro[j] + 1):
-                for i in range(tabuleiro[j]):
-                    print(chr(ord('a')+i), end=' ')
-                break
-    elif nome [j] == 'Bispo':
-            for linha in range (tabuleiro[j]):
-                print(n,end=' ')
-                n -=1
-                posicaoxl = (n+1) - posicao_num [j]
-                for coluna in range (2, (tabuleiro[j]+2), 1):
-                    if n+1 != posicao_num[j]:
-                        if (coluna - 1) == (posicao_letra[j] - 96 - posicaoxl):
-                            print('x', end= ' ')
-                        elif (coluna - 1) == (posicao_letra[j] - 96 + posicaoxl):
-                            print('x', end= ' ')
-                        else:
-                                print('-', end= ' ')
-                    else:
-                        if (coluna - 1) == (posicao_letra[j] - 96):
-                             print('o', end= ' ')
-                        else:
-                            print('-', end= ' ')
-                print ( )
-            print (' ', end=' ') 
-            for linha in range (tabuleiro[j] + 1):
-                for i in range(tabuleiro[j]):
-                    print(chr(ord('a')+i), end=' ')
-                break
-    elif nome [j] == 'Dama':
-        for linha in range (tabuleiro[j]):
-            print(n,end=' ')
-            n -=1
-            posicaoxl = n+1 - posicao_num [j]
-            for coluna in range (2, (tabuleiro[j]+2), 1):
-                if n+1 != posicao_num[j] and (coluna - 1) == (posicao_letra[j] - 96 - posicaoxl):
+            elif peca == "Dama":
+                posicaoxl = tabuleiro + 1 - posicao_linha 
+                if tabuleiro + 1 != posicao_linha and (coluna - 1) == (posicao_coluna_ord - 96 - posicaoxl):
                     print('x', end= ' ')
-                elif n+1 != posicao_num[j] and (coluna - 1) == (posicao_letra[j] - 96 + posicaoxl):
+                elif tabuleiro + 1 != posicao_linha and (coluna - 1) == (posicao_coluna_ord - 96 + posicaoxl):
                     print('x', end= ' ')
-                elif coluna != (posicao_letra[j] - 95) and posicao_num [j] == (n+1):
+                elif coluna != (posicao_coluna_ord - 95) and posicao_linha == (tabuleiro + 1):
                     print('x', end= ' ')
-                elif (n + 1) == posicao_num [j] and coluna == (posicao_letra[j] - 95):
+                elif (tabuleiro + 1) == posicao_linha and coluna == (posicao_coluna_ord - 95):
                     print('o', end= ' ')
-                elif coluna == (posicao_letra[j] - 95) and (n + 1) != posicao_num [j]:
+                elif coluna == (posicao_coluna_ord - 95) and (tabuleiro + 1) != posicao_linha:
                     print('x', end= ' ')
                 else:
                     print('-', end=' ')     
-            print ( )   
-        print (' ', end=' ')    
-        for linha in range (tabuleiro[j] + 1):
-            for i in range(tabuleiro[j]):
-                print(chr(ord('a')+i), end=' ')
-            break
-    elif nome[j] == 'Rei':
-            for linha in range (tabuleiro[j]):
-                print(n,end=' ')
-                n -=1
-                for coluna in range (2, (tabuleiro[j]+2), 1):
-                    if coluna == (posicao_letra[j] - 95) and linha == (tabuleiro[j] - posicao_num [j]):
-                        print ('o', end=' ')
-                    elif coluna == (posicao_letra [j] - 94) and linha == (tabuleiro[j] - posicao_num [j]):
-                        print ('x', end= ' ')
-                    elif coluna == (posicao_letra [j] - 96) and linha == (tabuleiro[j] - posicao_num [j]):
-                        print ('x', end= ' ')
-                    elif coluna == (posicao_letra [j] - 95) and linha == (tabuleiro[j] - posicao_num [j] + 1):
-                        print ('x', end= ' ')
-                    elif coluna == (posicao_letra [j] - 95) and linha == (tabuleiro[j] - posicao_num [j] - 1):
-                        print ('x', end= ' ')
-                    elif coluna == (posicao_letra [j] - 94) and linha == (tabuleiro[j] - posicao_num [j] + 1):
-                        print ('x', end= ' ')
-                    elif coluna == (posicao_letra [j] - 94) and linha == (tabuleiro[j] - posicao_num [j] - 1):
-                        print ('x', end= ' ')
-                    elif coluna == (posicao_letra [j] - 96) and linha == (tabuleiro[j] - posicao_num [j] + 1):
-                        print ('x', end= ' ')
-                    elif coluna == (posicao_letra [j] - 96) and linha == (tabuleiro[j] - posicao_num [j] - 1):
-                        print ('x', end= ' ')
-                    else:
+            elif peca == "Rei":
+                if coluna == (posicao_coluna_ord - 95) and linha == (n - posicao_linha):
+                    print ('o', end=' ')
+                elif coluna == (posicao_coluna_ord - 94) and linha == (n - posicao_linha):
+                    print ('x', end= ' ')
+                elif coluna == (posicao_coluna_ord - 96) and linha == (n - posicao_linha):
+                    print ('x', end= ' ')
+                elif coluna == (posicao_coluna_ord - 95) and linha == (n - posicao_linha + 1):
+                    print ('x', end= ' ')
+                elif coluna == (posicao_coluna_ord - 95) and linha == (n - posicao_linha - 1):
+                    print ('x', end= ' ')
+                elif coluna == (posicao_coluna_ord - 94) and linha == (n - posicao_linha + 1):
+                    print ('x', end= ' ')
+                elif coluna == (posicao_coluna_ord - 94) and linha == (n - posicao_linha - 1):
+                    print ('x', end= ' ')
+                elif coluna == (posicao_coluna_ord - 96) and linha == (n - posicao_linha + 1):
+                    print ('x', end= ' ')
+                elif coluna == (posicao_coluna_ord - 96) and linha == (n - posicao_linha - 1):
+                    print ('x', end= ' ')
+                else:
+                    print('-', end= ' ')
+            elif peca == "Cavalo":
+                if (posicao_linha + 2) < tabuleiro + 1 or tabuleiro + 1 < (posicao_linha - 2):
+                    print('-', end= ' ')
+                elif (posicao_linha + 2) == tabuleiro + 1 or (posicao_linha - 2) == tabuleiro + 1:
+                    if coluna != (posicao_coluna_ord - 96) and coluna != (posicao_coluna_ord - 94):
                         print('-', end= ' ')
-                print ( )
-            print (' ', end=' ')    
-            for linha in range (tabuleiro[j] + 1):
-                for i in range(tabuleiro[j]):
-                    print(chr(ord('a')+i), end=' ')
-                break
-    elif nome [j] == 'Cavalo':
-            for linha in range (tabuleiro[j]):
-                print(n,end=' ')
-                n -=1
-                for coluna in range (2, (tabuleiro[j]+2), 1):
-                    if (posicao_num [j] + 2) < n+1 or n+1 < (posicao_num [j] - 2):
-                        print('-', end= ' ')
-                    elif (posicao_num [j] + 2) == n+1 or (posicao_num [j] - 2) == n+1:
-                        if coluna != (posicao_letra[j] - 96) and coluna != (posicao_letra[j] - 94):
-                            print('-', end= ' ')
-                        else:
-                            print('x', end= ' ')
-                    elif (posicao_num [j] + 1) == n +1 or (posicao_num [j] - 1) == n +1:
-                        if coluna != (posicao_letra[j] - 97) and coluna != (posicao_letra[j] - 93):
-                            print('-', end= ' ')
-                        else:
-                            print('x', end= ' ')
-                    elif posicao_num [j] == n+1:
-                        if coluna != (posicao_letra[j] - 95):
-                            print('-', end= ' ')
-                        else:
-                            print('o', end= ' ')                  
                     else:
                         print('x', end= ' ')
-                print ( )
-            print (' ', end=' ')    
-            for linha in range (tabuleiro[j] + 1):
-                for i in range(tabuleiro[j]):
-                    print(chr(ord('a')+i), end=' ')
-                break
-    else: #Peao
-            for linha in range (tabuleiro[j]):
-                print(n,end=' ')
-                n -=1
-                for coluna in range (2, (tabuleiro[j]+2), 1):
-                    if posicao_num [j] == 2:
-                        if coluna == (posicao_letra[j] - 95) and linha == (tabuleiro[j] - posicao_num [j]):
-                            print ('o', end=' ')
-                        elif coluna == (posicao_letra [j] - 95) and linha == (tabuleiro[j] - posicao_num [j] -2):
-                            print ('x', end= ' ')
-                        elif coluna == (posicao_letra [j] - 95) and linha == (tabuleiro[j] - posicao_num [j] -1):
-                            print ('x', end= ' ')
-                        else:
-                            print('-', end= ' ')
+                elif (posicao_linha + 1) == tabuleiro + 1 or (posicao_linha - 1) == tabuleiro + 1:
+                    if coluna != (posicao_coluna_ord - 97) and coluna != (posicao_coluna_ord - 93):
+                        print('-', end= ' ')
                     else:
-                        if coluna == (posicao_letra[j] - 95) and linha == (tabuleiro[j] - posicao_num [j]):
-                            print ('o', end=' ')
-                        elif coluna == (posicao_letra [j] - 95) and linha == (tabuleiro[j] - posicao_num [j] -1):
-                            print ('x', end= ' ')
-                        else:
-                            print('-', end= ' ')
-                print ( )
-            print (' ', end=' ')    
-            for linha in range (tabuleiro[j] + 1):
-                for i in range(tabuleiro[j]):
-                    print(chr(ord('a')+i), end=' ')
-                break
-    j+= 1
-    print ( )
-    print ( )
+                        print('x', end= ' ')
+                elif posicao_linha == tabuleiro + 1:
+                    if coluna != (posicao_coluna_ord - 95):
+                        print('-', end= ' ')
+                    else:
+                        print('o', end= ' ')                  
+                else:
+                    print('x', end= ' ')
+            else: #Peao
+                if posicao_linha == 2:
+                    if coluna == (posicao_coluna_ord - 95) and linha == (n - posicao_linha):
+                        print ('o', end=' ')
+                    elif coluna == (posicao_coluna_ord - 95) and linha == (n - posicao_linha - 2):
+                        print ('x', end= ' ')
+                    elif coluna == (posicao_coluna_ord - 95) and linha == (n - posicao_linha - 1):
+                        print ('x', end= ' ')
+                    else:
+                        print('-', end= ' ')
+                else:
+                    if coluna == (posicao_coluna_ord - 95) and linha == (n - posicao_linha):
+                        print ('o', end=' ')
+                    elif coluna == (posicao_coluna_ord - 95) and linha == (n - posicao_linha - 1 ):
+                        print ('x', end= ' ')
+                    else:
+                        print('-', end= ' ')
+        print()
+    print(' ', end=' ')
+    for linha in range(n + 1):
+        for i in range(n):
+            print(chr(ord('a') + i), end=' ')
+        break
+
+    n = int(input())
